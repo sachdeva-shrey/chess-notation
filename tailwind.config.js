7bd41f388a46cd33218aad8dc9c9376b6e8fb348
sachdeva-shrey <shreysachdeva.2000@gmail.com>
2021-03-05 23:59:57 +0530

    🎉 initialise with CRA
    Signed-off-by: sachdeva-shrey <shreysachdeva.2000@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    theme: {
+      screens: {
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+    extend: {
+      opacity: ["disabled"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
